c3dc582518335ba2ab987e0e267afc074c4edd30
DailyMantova <93875893+DailyMantova@users.noreply.github.com>
2022-07-13 01:25:34 +0200

    added configurations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {NavBar} from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 const App = () => {
 
   return (
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,44 @@
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import Navbar from "react-bootstrap/Navbar"
 
 const NavBar = () => {
-	return (
-	<Button>Menu</Button>
-	)
-
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ToDoDaily</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Завдання</Nav.Link>
+              <Nav.Link href="#pricing">Список Справ</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Меню</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
 }
 export default NavBar
